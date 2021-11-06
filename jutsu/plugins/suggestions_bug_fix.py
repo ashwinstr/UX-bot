17ca839c48c74d7154fc8ff286c17c98bb9d63b1
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-06 13:10:00 +0530

    Update suggestions_bug_fix.py

--- jutsu/plugins/suggestions_bug_fix.py
+++ jutsu/plugins/suggestions_bug_fix.py
@@ -21,3 +21,22 @@ async def sug_log(bot, message):
         text_ = message.caption
         text_ += f"{text_}\n\n### PENDING ###"
         await bot.edit_message_caption(LOG_CHANNEL, msg_.message_id, text_)
+
+        
+@Client.on_message(
+    filters.chat([-1001331162912])
+    & filters.regex(pattern=r"^#bug"),
+    group=1
+)
+async def sug_log(bot, message):
+    mention = await bot.send_message(message.chat.id, f"Bug report by **{message.from_user.mention}** logged successfully.")
+    msg_ = await bot.copy_message(LOG_CHANNEL, message.chat.id, message.message_id, reply_to_message_id=mention.message_id)
+    await bot.send_message(message.chat.id, "`Your bug report has been logged, admins will look at it when they can...\nThank you.`", reply_to_message_id=message.message_id)
+    if message.text:
+        text_ = message.text
+        text_ += f"{text_}\n\n### PENDING ###"
+        await bot.edit_message_text(LOG_CHANNEL, msg_.message_id, text_)
+    elif message.caption:
+        text_ = message.caption
+        text_ += f"{text_}\n\n### PENDING ###"
+        await bot.edit_message_caption(LOG_CHANNEL, msg_.message_id, text_)
