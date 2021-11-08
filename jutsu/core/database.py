dbb3254f123d798acd8b40944b9dbc95d2596cd0
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-08 16:24:49 +0530

    Update database.py

--- jutsu/core/database.py
+++ jutsu/core/database.py
@@ -12,18 +12,17 @@ from jutsu.config import Config
 
 
 _MGCLIENT: AgnosticClient = AsyncIOMotorClient(Config.DB_URI)
-_RUN = asyncio.get_event_loop().run_until_complete
+try:
+    _RUN = asyncio.get_event_loop().run_until_complete
+except:
+    _RUN = asyncio.get_event_loop()
 
-_DATABASE: AgnosticDatabase = _MGCLIENT["Userge"]
+_DATABASE: AgnosticDatabase = _MGCLIENT["jutsu"]
 _COL_LIST: List[str] = _RUN(_DATABASE.list_collection_names())
 
 
 def get_collection(name: str) -> AgnosticCollection:
     """ Create or Get Collection from your database """
-    if name in _COL_LIST:
-        _LOG.debug(_LOG_STR, f"{name} Collection Found :) => Now Logging to it...")
-    else:
-        _LOG.debug(_LOG_STR, f"{name} Collection Not Found :( => Creating New Collection...")
     return _DATABASE[name]
 
 
