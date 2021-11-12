1d54579190acfd89cf039ea6d4bb44a7fa7bb7d0
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-12 14:20:16 +0530

    Update __init__.py

--- jutsu/__init__.py
+++ jutsu/__init__.py
@@ -3,11 +3,4 @@ from jutsu.core.database import get_collection
 from jutsu.config import Config
 from jutsu.helpers import int_list
 
-
-def load_adm():
-    with open("cache/admin_list.txt", "r") as list_:
-        adm_lst = list_.read()
-    _list = adm_lst.split()
-    _list = int_list(_list)
-    return _list
     
