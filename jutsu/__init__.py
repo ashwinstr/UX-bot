523527e5083f3783e9ba993377196ea8fe8a8502
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-12 13:00:10 +0530

    Update __init__.py

--- jutsu/__init__.py
+++ jutsu/__init__.py
@@ -3,19 +3,3 @@
 from jutsu.core.database import get_collection
 from jutsu.config import Config
 from jutsu.helpers.tools import telegrapher
-
-ADMINS = get_collection("ADMINS") 
-
-
-async def admin_list():
-    if not os.path.exists("cache/"):
-        os.mkdir("cache/")
-    list_ = []
-    found = await ADMINS.find_one({'chat_id': -1001331162912})
-    if found:
-        list_ = found['admin_ids']
-    owner = int(str(Config.OWNER_ID).split()[0])
-    list_.append(owner)
-    with open("cache/admin_list.txt", "w+") as adm_lst:
-        for one in list_:
-            adm_lst.write(f"{one} ")
