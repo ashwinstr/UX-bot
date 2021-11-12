e225d73dd8e8ea96de49b4340827ae608027786a
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-12 09:34:24 +0530

    Update warning.py

--- jutsu/plugins/warning.py
+++ jutsu/plugins/warning.py
@@ -22,7 +22,7 @@ async def _init() -> None:
         list_ = found['admin_ids']
     owner = int(str(Config.OWNER_ID).split()[0])
     list_.append(owner)
-    setattr(Config, "ADMINS", list_)
+    Config.ADMINS = list_
 
 
 @Client.on_message(
