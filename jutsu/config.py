d7eb064b20ff6e681ed0884b8eade4462dfe68d5
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-12 14:33:32 +0530

    Update config.py

--- jutsu/config.py
+++ jutsu/config.py
@@ -3,7 +3,10 @@ __all__ = ['Config']
 
 import os
 from typing import Set
+from jutsu import get_collection
 from jutsu.helpers import int_list 
+
+ADMINS = get_collection("ADMINS")
 
 class Config:
     DB_URI = os.environ.get("DATABASE_URL")
@@ -11,6 +14,9 @@ class Config:
     ADMINS = [] 
 
 class Admins:
+    async for data in ADMINS.find():
+        one = data['admin_ids']
+    admins = one
     with open("cache/admin_list.txt", "r") as list_:
         adm_lst = list_.read()
     _list = adm_lst.split()
