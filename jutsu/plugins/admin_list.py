1866b051e6c487e2cd7228cad20cc74e20407e5e
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-12 13:15:29 +0530

    Update admin_list.py

--- jutsu/plugins/admin_list.py
+++ jutsu/plugins/admin_list.py
@@ -73,3 +73,23 @@ async def list_admins(bot, message):
         return await msg.edit(out_)
     else:
         await msg.edit("`There are no admins in this chat.`")
+
+
+@Client.on_message(
+    filters.command(["admincache"], prefixes="?")
+    & filters.user([owner])
+    & filters.group,
+    group=4
+)
+async def admin_cache(bot, message):
+    msg = await bot.send_message(message.chat.id, "`Refreshing admin list...`", reply_to_message_id=message.message_id)
+    if not os.path.isdir("cache/"):
+        os.mkdir("cache/")
+    found = await ADMINS.find_one({'chat_id': -1001331162912})
+    if not found:
+        return await msg.edit("`List is empty.`")
+    list_ = found['admin_ids']
+    with open("cache/admin_list.txt", "w+") as admlist:
+        for one in list_:
+            admlist.write(f"{one} ")
+    await msg.edit("`Admin cache refreshed.`")
