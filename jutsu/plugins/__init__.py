dab0ec172fc0ddb30fec36f829a7dd0d99526ac5
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-15 17:17:29 +0530

    Create __init__.py

---
+++ jutsu/plugins/__init__.py
@@ -0,0 +1,18 @@
+
+import aiofiles
+import os
+from jutsu import get_collection
+
+ADMINS = get_collection("ADMINS")
+
+
+async def admin_cache():
+    if not os.path.isdir("cache/"):
+        os.mkdir("cache/")
+    found = await ADMINS.find_one({'chat_id': -1001331162912})
+    if not found:
+        return
+    list_ = found['admin_ids']
+    async with aiofiles.open("cache/admin_list.txt", "w+") as fn:
+        for one in list_:
+            await fn.writelines(f"{one} ")
