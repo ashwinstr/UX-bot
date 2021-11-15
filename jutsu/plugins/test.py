75abe6a02154cdbf83e7f4c0bef8fe1a41f33604
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-15 20:36:39 +0530

    Create test.py

---
+++ jutsu/plugins/test.py
@@ -0,0 +1,17 @@
+
+from pyrogram import filters, Client
+
+from jutsu import Config
+from jutsu.plugins.warning import _admins_list_
+
+owner = int(str(Config.OWNER_ID).split()[0])
+
+
+@Client.on_message(
+    filters.command(["testing"], prefixes="?")
+    & filters.group
+    & filters.user([one for one in _admins_list_()]),
+    group=7
+)
+async def test(bot, message):
+    await bot.send_message(message.chat.id, "Working", reply_to_message_id=message.message_id)
