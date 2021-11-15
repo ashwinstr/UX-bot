e45e647caddb25a9efade5c87e2108abbb9bb42d
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-15 19:19:41 +0530

    Create restart.py

---
+++ jutsu/plugins/restart.py
@@ -0,0 +1,18 @@
+
+import asyncio
+
+from pyrogram import filters, Client
+
+owner = int(str(Config.OWNER_ID).split()[0]) 
+
+
+@Client.on_message(
+    filters.command(["restart"], prefixes="?")
+    & filters.user([owner])
+    & filters.group,
+    group=5
+)
+async def restarting(bot, message):
+    msg = await bot.send_message(message.chat.id, "`Restating app...`", reply_to_message_id=message.message_id)
+    asyncio.get_event_loop().create_task(bot.restart())
+    await msg.edit("`App restarted.`")
