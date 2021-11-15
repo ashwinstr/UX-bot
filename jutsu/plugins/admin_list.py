fb54973db6bad03eac3514d69c35f648872a224b
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-15 15:50:38 +0530

    Update admin_list.py

--- jutsu/plugins/admin_list.py
+++ jutsu/plugins/admin_list.py
@@ -91,6 +91,6 @@ async def admin_cache(bot, message):
         return await msg.edit("`List is empty.`")
     list_ = found['admin_ids']
     async with aiofiles.open("cache/admin_list.txt", "w+") as fn:
-        fn.write(list_)
+        await fn.write(list_)
     await msg.edit("`Admin cache refreshed.`")
     asyncio.get_event_loop().create_task(bot.restart())
