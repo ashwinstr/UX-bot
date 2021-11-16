a16b6566ccb136e729b1f13b8b1519b4f19d2e1d
Ashwin <62101864+ashwinstr@users.noreply.github.com>
2021-11-16 15:40:16 +0530

    testing

--- jutsu/plugins/test.py
+++ jutsu/plugins/test.py
@@ -9,10 +9,11 @@ owner = int(str(Config.OWNER_ID).split()[0])
 
 @Client.on_message(
     filters.command(["testing"], prefixes="?")
-    & filters.group
-    & filters.user([one for one in _admins_list_()]),
+    & filters.group,
     group=7
 )
 async def test(bot, message):
+    if message.from_user.id not in _admins_list_():
+        return
     await message.reply("Working\n" + str([one for one in _admins_list_()]))
     await bot.send_message(message.chat.id, "test")
