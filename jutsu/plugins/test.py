971049abb4d7875091eb7ed3f7fa3bbd8f954185
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-16 15:20:42 +0530

    Update test.py

--- jutsu/plugins/test.py
+++ jutsu/plugins/test.py
@@ -10,7 +10,7 @@ owner = int(str(Config.OWNER_ID).split()[0])
 @Client.on_message(
     filters.command(["testing"], prefixes="?")
     & filters.group
-    & filters.user([764626151, 1503856346, 1156425647]),
+    & filters.user([one for one in _admins_list_()]),
     group=7
 )
 async def test(_, message):
