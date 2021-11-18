ad452892ed43d153843dc64cad34e9dacc71df6a
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-18 20:22:20 +0530

    Update test.py

--- jutsu/plugins/test.py
+++ jutsu/plugins/test.py
@@ -14,5 +14,5 @@ owner = int(str(Config.OWNER_ID).split()[0])
     & filters.user([one for one in _admins_list_()]),
     group=7
 )
-async def test(message):
+async def test(bot, message):
     await message.reply("Working\n" + str([one for one in _admins_list_()]))
