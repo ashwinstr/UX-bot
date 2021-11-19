0eca7fd39c96872f052336f7015ee00c6b9442f2
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-19 10:39:46 +0530

    Update test.py

--- jutsu/plugins/test.py
+++ jutsu/plugins/test.py
@@ -3,16 +3,17 @@ from pyrogram import filters, Client
 
 from jutsu import Config, app
 from jutsu.plugins.warning import _admins_list_
+from .admins import admins
 
 owner = int(str(Config.OWNER_ID).split()[0])
 
 
-admins = filters.create(_admins_list_)
+admins_ = filters.create(admins)
 
 @app.on_message(
     filters.command(["testing"], prefixes="?")
     & filters.group
-    & admins,
+    & admins_,
     group=7
 )
 async def test(bot, message):
