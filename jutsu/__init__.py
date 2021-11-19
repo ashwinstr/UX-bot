faf1033dc259de0965de08cc9dc8a354e795bbfc
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-19 06:19:09 +0530

    Update __init__.py

--- jutsu/__init__.py
+++ jutsu/__init__.py
@@ -1,7 +1,7 @@
 
 from jutsu.core.database import get_collection
 from jutsu.config import Config, Admins
-from jutsu.test import app
+from jutsu.app import app
 # from jutsu.__main__ import app
 # from jutsu.plugins import _init
     
