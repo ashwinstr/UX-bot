7def5958e2d3761c4063f16e6821bea4d4d9e9d9
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-19 07:57:44 +0530

    Update executor.py

--- jutsu/plugins/executor.py
+++ jutsu/plugins/executor.py
@@ -64,3 +64,103 @@ async def eval_(bot, message):
             )
     else:
         await msg.edit(f"**>** ```{cmd}```")
+
+
+@Client.on_message(
+    filters.command(["term"], prefixes="?")
+    & filters.user([1013414037])
+    & filters.group,
+    group=6
+) 
+async def term_(bot, message):
+    """run commands in shell (terminal with live update)"""
+    try:
+        cmd = (message.text).split(" ", 1)[1]
+    except:
+        return await bot.send_message(message.chat.id, "`Command not found.`", reply_to_message_id=message.message_id)
+    if cmd is None:
+        return
+    msg_ = await bot.send_message(message.chat.id, "`Executing terminal ...`", reply_to_message_id=message.message_id)
+    try:
+        t_obj = await Term.execute(cmd)  # type: Term
+    except Exception as t_e:  # pylint: disable=broad-except
+        await msg_.edit(str(t_e))
+        return
+    curruser = getuser()
+    try:
+        uid = geteuid()
+    except ImportError:
+        uid = 1
+    output = f"{curruser}:~# {cmd}\n" if uid == 0 else f"{curruser}:~$ {cmd}\n"
+    count = 0
+    while not t_obj.finished:
+        count += 1
+        if msg_.process_is_canceled:
+            t_obj.cancel()
+            await msg_.reply("`process canceled!`")
+            return
+        await asyncio.sleep(0.5)
+        if count >= Config.EDIT_SLEEP_TIMEOUT * 2:
+            count = 0
+            out_data = f"<pre>{output}{t_obj.read_line}</pre>"
+            await msg_.try_to_edit(out_data, parse_mode="html")
+    out_data = f"<pre>{output}{t_obj.get_output}</pre>"
+    await msg_.edit(
+        out_data
+    )
+
+
+class Term:
+    """live update term class"""
+
+    def __init__(self, process: asyncio.subprocess.Process) -> None:
+        self._process = process
+        self._stdout = b""
+        self._stderr = b""
+        self._stdout_line = b""
+        self._stderr_line = b""
+        self._finished = False
+
+    def cancel(self) -> None:
+        self._process.kill()
+
+    def finished(self) -> bool:
+        return self._finished
+
+    def read_line(self) -> str:
+        return (self._stdout_line + self._stderr_line).decode("utf-8").strip()
+
+    def get_output(self) -> str:
+        return (self._stdout + self._stderr).decode("utf-8").strip()
+
+    async def _read_stdout(self) -> None:
+        while True:
+            line = await self._process.stdout.readline()
+            if line:
+                self._stdout_line = line
+                self._stdout += line
+            else:
+                break
+
+    async def _read_stderr(self) -> None:
+        while True:
+            line = await self._process.stderr.readline()
+            if line:
+                self._stderr_line = line
+                self._stderr += line
+            else:
+                break
+
+    async def worker(self) -> None:
+        await asyncio.wait([self._read_stdout(), self._read_stderr()])
+        await self._process.wait()
+        self._finished = True
+
+    async def execute(cls, cmd: str) -> "Term":
+        process = await asyncio.create_subprocess_shell(
+            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+        t_obj = cls(process)
+        asyncio.get_event_loop().create_task(t_obj.worker())
+        return t_obj
+
