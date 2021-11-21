71c75163f1a9b8c2ec18778b9f3404f51cdf2147
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-21 14:35:51 +0530

    Update admins.py

--- jutsu/plugins/admins.py
+++ jutsu/plugins/admins.py
@@ -1,16 +1,1 @@
-from jutsu import get_collection
 
-
-ADMINS = get_collection("ADMINS")
-
-
-async def Admins(_, __, message) -> bool:
-    await message.reply(message.from_user.id)
-    async for data in ADMINS.find():
-        list_ = data['admin_ids']
-        break
-    if message.from_user.id in list_:
-        print("True")
-        return True
-    print("False")
-    return False
