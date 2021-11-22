5b4f1170074231813ac1b226e579443d77e50f12
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-22 16:02:35 +0530

    Update __main__.py

--- jutsu/__main__.py
+++ jutsu/__main__.py
@@ -1,6 +1,7 @@
 
 import os
 import pyrogram
+from pyrogram import Client
 from decouple import config
 
 
@@ -9,12 +10,20 @@ APP_ID = config("APP_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 
+async def genStrSession():  # pylint: disable=missing-function-docstring
+    async with Client(
+            "Jutsu",
+            api_id=APP_ID,
+            api_hash=API_HASH,
+    ) as jutsu:
+        string = await userge.export_session_string()
+    return string
 
 if __name__ == "__main__" :
     print("### Starting Bot... ###")
     plugins = dict(root="jutsu/plugins")
     app = pyrogram.Client(
-        "sharingan",
+        await genStrSession(),
         bot_token=BOT_TOKEN,
         api_id=APP_ID,
         api_hash=API_HASH,
