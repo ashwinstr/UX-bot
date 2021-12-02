94d60dfeeaed31d0d96a86a39aadbb9a019edc8a
Kakashi <ashwinstr@gmail.com>
2021-12-02 15:23:56 +0530

    fixes

--- jutsu/config.py
+++ jutsu/config.py
@@ -2,19 +2,8 @@ __all__ = ['Config']
 
 
 import os
-from typing import Set
-from jutsu.helpers import int_list
 
 
 class Config:
     DB_URI = os.environ.get("DATABASE_URL")
-    OWNER_ID = os.environ.get("OWNER_ID")
-    ADMINS = [] 
-
-class Admins:
-    def admins_list_():
-        with open("cache/admin_list.txt", "r") as list_:
-            adm_lst = list_.read()
-        adm_lst = adm_lst.split()
-        _list = int_list(adm_lst)
-        return _list
+    OWNER_ID = int(os.environ.get("OWNER_ID"))
