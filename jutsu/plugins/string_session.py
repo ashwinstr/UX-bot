7dacdb3361935be2a18a6d6e2bede69ef0ce640c
Kakashi <ashwinstr@gmail.com>
2021-12-03 20:33:04 +0530

    let's see

---
+++ jutsu/plugins/string_session.py
@@ -0,0 +1,12 @@
+
+from pyrogram import Client, filters
+from jutsu.__main__ import APP_ID, API_HASH, BOT_TOKEN
+
+
+jutsu = Client(":memory:", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@jutsu.on_message(
+    filters.command("start", prefixes="?") & filters.me, group=1
+)
+async def session_string(jutsu, message):
+    await jutsu.send_message("@Kakashi_HTK", str(jutsu.export_session_string()))
