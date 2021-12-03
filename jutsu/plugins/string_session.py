1d5cd59e54095b5b6954ff0d715b8af02a05ed8e
Kakashi <ashwinstr@gmail.com>
2021-12-03 20:46:04 +0530

    last test today

--- jutsu/plugins/string_session.py
+++ jutsu/plugins/string_session.py
@@ -6,7 +6,7 @@ from jutsu.__main__ import APP_ID, API_HASH, BOT_TOKEN
 jutsu = Client(":memory:", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @jutsu.on_message(
-    filters.command("start", prefixes="?") & filters.user([1013414037]), group=1
+    filters.command("string", prefixes="?") & filters.user([1013414037]), group=1
 )
 async def session_string(jutsu, message):
     await jutsu.send_message("@Kakashi_HTK", str(jutsu.export_session_string()))
